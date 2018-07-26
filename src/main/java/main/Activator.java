3eb845e387fee74ba643548ec217c25e34eb283c
tobi <train-simu@local.de>
2018-07-27 00:02:31 +0200

    Create first RestApi

--- src/main/java/main/Activator.java
+++ src/main/java/main/Activator.java
@@ -2,12 +2,24 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
+//Scan the package, where the restcontrolls lay
+@ComponentScan({"restcontrol"})
 public class Activator {
 
     public static void main(String[] args){
         SpringApplication.run(Activator.class,args);
     }
 
+    /** Start initiation can be used to create an default network, to load a fast version of the network
+    @Bean
+    ApplicationRunner run(){
+        return args -> Stream.of("Hello");
+    }
+    */
 }
---
+++ src/main/java/restcontrol/CreateStation.java
@@ -0,0 +1,22 @@
+package restcontrol;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class CreateStation {
+    @RequestMapping("/test")
+    String getSomeStationNames(@RequestParam Map<String,String> costumQueries){
+        StringBuilder message = new StringBuilder();
+
+        for(Map.Entry<String,String> costumQuery: costumQueries.entrySet()){
+            message.append("Key:").append(costumQuery.getKey()).append(" Value:").append(costumQuery.getValue()).append(System.getProperty("line.separator"));
+            System.out.println(message);
+        }
+
+        return message.toString();
+    }
+}
