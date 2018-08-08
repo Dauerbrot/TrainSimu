d36b0e4e725d44669a6645fe6466c3b04df6e85a
tobi <train-simu@local.de>
2018-08-08 18:03:13 +0200

    +Adding angularjs directive for navigationbar

--- src/main/resources/public/option/navbar.js
+++ src/main/resources/public/option/navbar.js
@@ -1,6 +1,12 @@
 app.directive("navigationList",function(){
+    var controller = ['$scope', function($scope){
+        console.log($scope);
+    }];
+
     return {
-        templateUrl: "/option/navbar.html"
+        restrict: 'EA',
+        templateUrl: "/option/navbar.html",
+        controller: controller
     }
 
 })
